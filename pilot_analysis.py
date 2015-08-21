d7020fcad5d921dd85aeadc7c6fcf1ef3b51a58b
bwallace <byron.wallace@gmail.com>
2015-08-21 10:57:09 -0500

    more progress?

--- pilot_analysis.py
+++ pilot_analysis.py
@@ -4,7 +4,7 @@ import sklearn
 
 import annotator_rationales as ar
 
-
+STOP_WORDS = [l.replace("\n", "") for l in open("pubmed.stoplist", 'rU').readlines()]
 HEADERS = ['workerId', 'experimentId', 'hitId', 'documentId', 'q1', 'q2', 'q3', 'q4', 'q1keys', 'q2keys', 'q3keys', 'q4keys', 'q1cust', 'q2cust', 'q3cust', 'q4cust', 'q1_norationales_expl', 'q2_norationales_expl', 'q3_norationales_expl', 'q4_norationales_expl', 'q1_norationales_reverse', 'q2_norationales_reverse', 'q3_norationales_reverse', 'q4_norationales_reverse', 'comments', 'honeypotId', 'honeypotPass', 'qualificationTest', 'timeUsed', 'ts']
 
 def load_pilot_annotations(annotations_path="pilot-data/pilotresults.csv"):
@@ -18,7 +18,7 @@ def load_texts_and_pmids(citations_and_labels_path="pilot-data/appendicitis.csv"
     for title, abstract in zip(appendicitis["title"], appendicitis["abstract"]):
         title_tokens =  word_tokenize(title)
         abstract_tokens = word_tokenize(abstract)
-        cur_text = ["TITLE"+t for t in title_tokens]
+        cur_text = ["TITLE"+t for t in title_tokens if t not in STOP_WORDS]
         cur_text.extend(abstract_tokens)
         texts.append(cur_text)
 
@@ -28,23 +28,32 @@ def load_texts_and_pmids(citations_and_labels_path="pilot-data/appendicitis.csv"
 def read_lbls(labels_path="pilot-data/labels.csv"):
     lbls = pd.read_csv(labels_path)
     # all of these pmids were screened in at the citation level.
-    lvl1_set = lbls[""]
+    lvl1_set = lbls["abstrackr_decision"]
+    lvl2_set = lbls["include?"]
+    return lvl1_set, lvl2_set
 
+def get_q_rationales(data, qnum):
+    pos_rationales = data[data["q%s"%qnum]=="Yes"]["q%skeys" % qnum].values
+    neg_rationales = data[data["q%s"%qnum]=="No"]["q%skeys" % qnum].values
+    return pos_rationales, neg_rationales
 
 def rationales_exp():
     ##
     # basics: just load in the data + labels, vectorize
+    annotations = load_pilot_annotations()
     texts, pmids = load_texts_and_pmids()
     vectorizer = TfidfVectorizer(stop_words="english", min_df=3, max_features=50000)
     X = vectorizer.fit_transform(texts)
-    lvl1_lbls, lvl2_lbls = read_lbls()
-    for question in range(1,5):
-        pass
-        #y = np.array(labels)
+    # these are sets of pmids that indicate positive instances;
+    # all other instances are negative. these are final, abstract
+    # level decisions (i.e., aggregate over the sub-questions)
+
+    lvl1_pmids, lvl2_pmids = read_lbls()
+    for question_num in range(1,5):
 
         ##
         # now load in and encode the rationales
-        pos_rationales = _load_rationales(pos_rationales_path)
+        pos_rationales, neg_rationales = get_q_rationales(annotations, question_num)
         X_pos_rationales = vectorizer.transform(pos_rationales)
 
         neg_rationales = _load_rationales(neg_rationales_path)
